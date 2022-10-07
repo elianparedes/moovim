fbb32bb64a210fe1ad2a1eebf3fd1daf95d61fd9
Juan Burda <jburda@itba.edu.ar>
2022-10-07 15:00:44 -0300

    feat: pinia setup

---
+++ src/store/exercises.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+
+export const useExercisesStore = defineStore('exercises', {
+    state: () => ({ exercises: []}),
+    getters: {
+
+
+
+
+    },
+    actions: {
+
+
+
+
+
+    },
+});
