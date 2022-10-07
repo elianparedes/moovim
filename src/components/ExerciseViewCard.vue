197bf6dc97e8d625da80bce8dc66623e3bde6f56
Elian Paredes <elianparedes.dev@gmail.com>
2022-10-07 02:17:28 -0300

    fix: exercise card panels margins

--- src/components/ExerciseViewCard.vue
+++ src/components/ExerciseViewCard.vue
@@ -1,89 +1,81 @@
 <template>
-  <v-expansion-panels>
-      <v-expansion-panel>
-      <v-expansion-panel-header class="pb-0">
-        <v-row align="center" class="text-body-1">
-          <v-col cols="4">
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{exercise}}
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                  {{category}}
-                </v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-          <v-col cols="2">
-            {{'S' + sessionNumber++}}
-          </v-col>
-          <v-col cols="2">
-            {{sessions[0].reps}}
-          </v-col>
-          <v-col cols="2">
-            {{sessions[0].weight}}
-          </v-col>
-          <v-col cols="2">
-            {{sessions[0].secs}}
-          </v-col>
-          <v-spacer>
+  <v-expansion-panel style="background-color: #252525">
+    <v-expansion-panel-header>
+      <v-row align="center" class="text-body-1">
+        <v-col cols="4">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6 font-weight-bold">
+                {{ exercise }}
+              </v-list-item-title>
+              <v-list-item-subtitle class="mt-2">
+                {{ category }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+        <v-spacer> </v-spacer>
 
-          </v-spacer>
-        </v-row>
-      </v-expansion-panel-header>
-      <v-expansion-panel-content class="pr-6">
-        <v-row v-for="session in sessions" :key="session">
-          <v-col cols="4">
+        <v-col cols="1" class="text-h6 font-weight-light">
+          {{ "S" + 1 }}
+        </v-col>
+        <v-col cols="1" class="text-h6 font-weight-light">
+          {{ sessions[0].reps }}
+        </v-col>
+        <v-col cols="1" class="text-h6 font-weight-light">
+          {{ sessions[0].weight }}
+        </v-col>
+        <v-col cols="1" class="text-h6 font-weight-light">
+          {{ sessions[0].secs }}
+        </v-col>
 
-          </v-col>
-          <v-col cols="2">
-            {{'S' + sessionNumber++}}
-          </v-col>
-          <v-col cols="2">
-            {{session.reps}}
-          </v-col>
-          <v-col cols="2">
-            {{session.weight}}
-          </v-col>
-          <v-col cols="2">
-            {{session.secs}}
-          </v-col>
-          <v-spacer>
-
-          </v-spacer>
-        </v-row>
-
-      </v-expansion-panel-content>
+        <v-spacer> </v-spacer>
+      </v-row>
+    </v-expansion-panel-header>
+    <v-expansion-panel-content class="pr-6">
+      <v-row v-for="(session, n) in sessions" :key="session">
+        <v-col cols="4"> </v-col>
+        <v-spacer> </v-spacer>
+        <v-col cols="1" class="text-h6 font-weight-light">
+          {{ "S" + (n + 2) }}
+        </v-col>
+        <v-col cols="1" class="text-h6 font-weight-light">
+          {{ session.reps }}
+        </v-col>
+        <v-col cols="1" class="text-h6 font-weight-light">
+          {{ session.weight }}
+        </v-col>
+        <v-col cols="1" class="text-h6 font-weight-light">
+          {{ session.secs }}
+        </v-col>
+        <v-spacer> </v-spacer>
+      </v-row>
+    </v-expansion-panel-content>
   </v-expansion-panel>
-  </v-expansion-panels>
-
 </template>
 
 <script>
-  export default{
-    name:'ExerciseViewCard',
-    props: {
-      exercise:{
-        type: String,
-        required: true
-      },
-      category:{
-        type: String,
-        required:true
-      },
-      sessions:{
-        type: Array,
-        required: true
-      }
+export default {
+  name: "ExerciseViewCard",
+  props: {
+    exercise: {
+      type: String,
+      required: true,
     },
-    data(){
-      return {
-        sessionNumber : 1
-      }
+    category: {
+      type: String,
+      required: true,
     },
-    methods(){
-
-    }
-  };
+    sessions: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      sessionNumber: 1,
+    };
+  },
+  methods() {},
+};
 </script>
