18855f85772313a42db85681c25fdedb185a75c5
Juan Burda <jburda@itba.edu.ar>
2022-10-08 23:40:40 -0300

    new: routineCycleStore

---
+++ src/api/routineCycle.js
@@ -0,0 +1,43 @@
+import {Api} from "./api"
+
+export {routineCycleApi, RoutineCycle};
+
+class routineCycleApi{
+
+    static getUrl(routineId, cycleId){
+        return `${Api.baseUrl}/routines${routineId ? `/${routineId}` : "" }
+        /cycles${cycleId ? `/${cycleId}` : "" }`;
+    }
+
+    static async getAll(routineId, controller){
+        return await Api.get(routineCycleApi.getUrl(routineId), true, controller);
+    }
+
+    static async add(routineCycle, controller){
+        return await Api.post(routineCycleApi.getUrl(routineCycle.id), true, routineCycle, controller);
+    }
+
+    static async get(routineId, cycleId, controller){
+        return await Api.get(routineCycleApi.getUrl(routineId, cycleId), true, controller);
+    }
+
+    static async modify(routineId, fullRoutineCycle, controller){
+        return await Api.put(routineCycleApi.getUrl(routineId, fullRoutineCycle.id), true, fullRoutineCycle, controller);
+    }
+
+    static async delete(routineId, cycleId, controller){
+        return await Api.delete(routineCycleApi.getUrl(routineId, cycleId), true, controller);
+    }
+
+}
+
+class RoutineCycle{
+    // type = [warmup, exercise, cooldown]
+    constructor(name, detail, type, repetitions, metadata){
+        this.name=name;
+        this.detail=detail;
+        this.type=type;
+        this.repetitions=repetitions;
+        this.metadata=metadata;
+    }
+}
---
+++ src/stores/routineCycleStore.js
@@ -0,0 +1,67 @@
+import {defineStore} from "pinia"
+import {routineCycleApi, RoutineCycle} from "@/api/routineCycle"
+
+export {useRoutineCycleStore, FullRoutineCycle};
+
+const useRoutineCycleStore = defineStore("routine", {
+    state: () => ({items: []}),
+    getters:{
+
+    },
+    actions: {
+        push(fullRoutineCycle){
+            this.items.push(fullRoutineCycle);
+        },
+        replace(index, fullRoutineCycle){
+            this.items[index] = fullRoutineCycle;
+        },
+        splice(index) {
+            this.items.splice(index, 1);
+        },
+        async getAllRoutineCycles(routineId){
+            const result = await routineCycleApi.getAll(routineId);
+            return result;
+        },
+        async addRoutineCycle(name, detail, type, repetitions, metadata){
+            const result = await routineCycleApi.add(new RoutineCycle(name, detail, type, repetitions, metadata));
+            this.items.push(result);
+            return result;
+        },
+        async getRoutineCycle(routineId ,fullRoutineCycle){
+            const index = this.items.findIndex(fullRoutineCycle);
+            if (index >= 0){
+                return this.items[index];
+            }
+            const result = await routineCycleApi.get(routineId, fullRoutineCycle.id);
+            this.items.push(result);
+            return result;
+        },
+        async modifyRoutineCycle(routineId, fullRoutineCycle){
+            const result = await routineCycleApi.modify(routineId, fullRoutineCycle);
+            const index = this.items.findIndex(fullRoutineCycle);
+            if (index >= 0){
+                this.replace(index, result);
+            }
+            return result;
+        },
+        async deleteRoutineCycle(routineId, fullRoutineCycle){
+            await routineCycleApi.delete(routineId,fullRoutineCycle.id);
+            const index = this.items.findIndex(fullRoutineCycle);
+            if (index >= 0){
+                return this.splice(index);
+            }
+        }
+    }
+})
+
+class FullRoutineCycle{
+    // type = [warmup, exercise, cooldown]
+    constructor(id, name, detail, type, repetitions, metadata){
+        this.id=id;
+        this.name=name;
+        this.detail=detail;
+        this.type=type;
+        this.repetitions=repetitions;
+        this.metadata=metadata;
+    }
+}
