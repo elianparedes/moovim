a425eca6f14bbcba8a0f8d75e66f48b627e8affa
Elian Paredes <elianparedes.dev@gmail.com>
2022-10-11 00:06:31 -0300

    fix: routine cycle store unique identifier and cycle exercise url

--- src/api/exerciseCycle.js
+++ src/api/exerciseCycle.js
@@ -1,43 +1,59 @@
-import {Api} from "./api"
+import { Api } from "./api";
 
-export {cycleExercise, exerciseCycleApi};
+export { cycleExercise, exerciseCycleApi };
 
-class exerciseCycleApi{
+class exerciseCycleApi {
+  static getUrl(cycleId, exerciseId) {
+    return `${Api.baseUrl}/cycles/${cycleId ? `${cycleId}` : ""}
+        /exercises${exerciseId ? `/${exerciseId}` : ""}`;
+  }
 
-    static getUrl(cycleId, exerciseId){
-        return `${Api.baseUrl}/cycles/${cycleId ? `/${cycleId}` : "" }
-        /exercises${exerciseId ? `/${exerciseId}` : "" }`;
-    }
+  static async getAll(cycleId, controller) {
+    return await Api.get(exerciseCycleApi.getUrl(cycleId), true, controller);
+  }
 
-    static async getAll(cycleId, controller){
-        return await Api.get(exerciseCycleApi.getUrl(cycleId), true, controller);
-    }
+  static async get(cycleId, exerciseId, controller) {
+    return await Api.get(
+      exerciseCycleApi.getUrl(cycleId, exerciseId),
+      true,
+      controller
+    );
+  }
 
-    static async get(cycleId, exerciseId , controller){
-        return await Api.get(exerciseCycleApi.getUrl(cycleId,exerciseId), true, controller);
-    }
+  //add cycle exercise
+  static async add(cycleId, exerciseId, cycleExercise, controller) {
+    return await Api.post(
+      exerciseCycleApi.getUrl(cycleId, exerciseId),
+      true,
+      cycleExercise,
+      controller
+    );
+  }
 
-    //add cycle exercise
-    static async add(cycleId, exerciseId, cycleExercise, controller){
-        return await Api.post(exerciseCycleApi.getUrl(cycleId, exerciseId), true, cycleExercise, controller);
-    }
+  //modify cycle exercise
+  static async modify(cycleId, exerciseId, cycleExercise, controller) {
+    return await Api.put(
+      exerciseCycleApi.getUrl(cycleId, exerciseId),
+      true,
+      cycleExercise,
+      controller
+    );
+  }
 
-    //modify cycle exercise
-    static async modify(cycleId, exerciseId, cycleExercise, controller){
-        return await Api.put(exerciseCycleApi.getUrl(cycleId, exerciseId), true, cycleExercise, controller);
-    }
-
-    //delete cycle exercise
-    static async delete(cycleId, exerciseId, controller){
-        return await Api.delete(exerciseCycleApi.getUrl(cycleId, exerciseId), true, controller);
-    }
-
+  //delete cycle exercise
+  static async delete(cycleId, exerciseId, controller) {
+    return await Api.delete(
+      exerciseCycleApi.getUrl(cycleId, exerciseId),
+      true,
+      controller
+    );
+  }
 }
 
-class cycleExercise{
-    constructor(order, duration, repetitions){
-        this.order=order;
-        this.duration=duration;
-        this.repetitions=repetitions;
-    }
+class cycleExercise {
+  constructor(order, duration, repetitions) {
+    this.order = order;
+    this.duration = duration;
+    this.repetitions = repetitions;
+  }
 }
--- src/stores/routineStore.js
+++ src/stores/routineStore.js
@@ -1,68 +1,77 @@
-import {defineStore} from "pinia"
-import {routineApi, Routine} from "@/api/routine"
+import { defineStore } from "pinia";
+import { routineApi, Routine } from "@/api/routine";
 
-export {useRoutineStore, FullRoutine};
+export { useRoutineStore, FullRoutine };
 
 const useRoutineStore = defineStore("routine", {
-    state: () => ({items: []}),
-    getters:{
-
+  state: () => ({ items: [] }),
+  getters: {},
+  actions: {
+    push(fullRoutine) {
+      this.items.push(fullRoutine);
     },
-    actions: {
-        push(fullRoutine){
-            this.items.push(fullRoutine);
-        },
-        replace(index, fullRoutine){
-            this.items[index] = fullRoutine;
-        },
-        splice(index) {
-            this.items.splice(index, 1);
-        },
-        async addRoutine(name, detail, isPublic, category, difficulty){
-            const result = await routineApi.add(new Routine(name, detail, isPublic, category, difficulty));
-            this.items.push(result);
-            return result;
-        },
-        async getRoutine(fullRoutine){
-            const index = this.items.findIndex(fullRoutine);
-            if (index >= 0){
-                return this.items[index];
-            }
-            const result = await routineApi.get(fullRoutine.id);
-            this.items.push(result);
-            return result;
-        },
-        async getAllRoutine(){
-            const result = await routineApi.getAll();
-            return result;
-        },
-        async modifyRoutine(fullRoutine){
-            const result = await routineApi.modify(fullRoutine);
-            const index = this.items.findIndex(fullRoutine);
-            if (index >= 0){
-                this.replace(index, result);
-            }
-            return result;
-        },
-        async deleteRoutine(fullRoutine){
-            await routineApi.delete(fullRoutine.id);
-            const index = this.items.findIndex(fullRoutine);
-            if (index >= 0){
-                return this.splice(index);
-            }
-        }
-    }
-})
+    replace(index, fullRoutine) {
+      this.items[index] = fullRoutine;
+    },
+    splice(index) {
+      this.items.splice(index, 1);
+    },
+    async addRoutine(name, detail, isPublic, category, difficulty) {
+      const result = await routineApi.add(
+        new Routine(name, detail, isPublic, category, difficulty)
+      );
+      this.items.push(result);
+      return result;
+    },
+    async getRoutine(routineId) {
+      /*const index = this.items.findIndex(fullRoutine);
+      if (index >= 0) {
+        return this.items[index];
+      }*/
+      const result = await routineApi.get(routineId);
+      //this.items.push(result);
+      return result;
+    },
+    async getAllRoutine() {
+      const result = await routineApi.getAll();
+      return result;
+    },
+    async modifyRoutine(fullRoutine) {
+      const result = await routineApi.modify(fullRoutine);
+      const index = this.items.findIndex(fullRoutine);
+      if (index >= 0) {
+        this.replace(index, result);
+      }
+      return result;
+    },
+    async deleteRoutine(fullRoutine) {
+      await routineApi.delete(fullRoutine.id);
+      const index = this.items.findIndex(fullRoutine);
+      if (index >= 0) {
+        return this.splice(index);
+      }
+    },
+  },
+});
 
-class FullRoutine{
-    constructor(id, name, detail, isPublic, category, difficulty, user, metadata){
-        this.id=id;
-        this.name=name;
-        this.detail=detail;
-        this.isPublic=isPublic;
-        this.category=category;
-        this.difficulty=difficulty;
-        this.user=user;
-        this.metadata=metadata;
-    }
+class FullRoutine {
+  constructor(
+    id,
+    name,
+    detail,
+    isPublic,
+    category,
+    difficulty,
+    user,
+    metadata
+  ) {
+    this.id = id;
+    this.name = name;
+    this.detail = detail;
+    this.isPublic = isPublic;
+    this.category = category;
+    this.difficulty = difficulty;
+    this.user = user;
+    this.metadata = metadata;
+  }
 }
