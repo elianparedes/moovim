a82178e713c0bbde2df1c6007593e57fb4918e75
Juan Burda <jburda@itba.edu.ar>
2022-10-15 21:24:10 -0300

    new: favoritesApi and store

---
+++ src/api/favorites.js
@@ -0,0 +1,22 @@
+import {Api} from "./api"
+
+export {favoritesApi}
+
+class favoritesApi{
+
+    static getUrl(slug){
+        return `${Api.baseUrl}/favorites${slug ? `/${slug}` : "" }`;
+    }
+
+    static async getAll(controller){
+        return await Api.get(favoritesApi.getUrl(), true, controller);
+    }
+
+    static async add(routineId, controller){
+        return await Api.post(favoritesApi.getUrl(routineId), true, controller);
+    }
+
+    static async delete(routineId, controller){
+        return await Api.delete(favoritesApi.getUrl(routineId), true, controller);
+    }
+}
